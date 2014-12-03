35aa8a56cf2a1d3e9d02e6d9da8ccad6272db0ca
Carl1101 <carlosvazquez014@gmail.com>
2014-12-03 17:57:19 -0400

    Minor Changes

--- Farrow/gen/com/ceg/farrow/R.java
+++ Farrow/gen/com/ceg/farrow/R.java
@@ -1257,6 +1257,7 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int accept=0x7f0a0016;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int description=0x7f0a0012;
@@ -1264,8 +1265,8 @@ containing a value of this type.
         public static final int logIn=0x7f0a000f;
         public static final int password=0x7f0a0011;
         public static final int send=0x7f0a0015;
-        public static final int server_ip=0x7f0a0016;
-        public static final int server_port=0x7f0a0017;
+        public static final int server_ip=0x7f0a0017;
+        public static final int server_port=0x7f0a0018;
         public static final int title_activity_chat=0x7f0a0013;
         public static final int username=0x7f0a0010;
     }
